ee4027f098d1f78492bb98b7deb2f4535d5a82ac
GotardPL <43497104+GotardPL@users.noreply.github.com>
2021-02-23 13:11:02 +0100

    Added Gangsta's Paradise music disc

---
+++ src/main/java/net/mcreator/allahmod/item/GangstasParadiseItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.allahmod.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.MusicDiscItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+
+import net.mcreator.allahmod.AllahModModElements;
+
+import java.util.List;
+
+@AllahModModElements.ModElement.Tag
+public class GangstasParadiseItem extends AllahModModElements.ModElement {
+	@ObjectHolder("allah_mod:gangstas_paradise")
+	public static final Item block = null;
+	public GangstasParadiseItem(AllahModModElements instance) {
+		super(instance, 6);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new MusicDiscItemCustom());
+	}
+	public static class MusicDiscItemCustom extends MusicDiscItem {
+		public MusicDiscItemCustom() {
+			super(0, AllahModModElements.sounds.get(new ResourceLocation("allah_mod:music_gangstasparadise")),
+					new Item.Properties().group(ItemGroup.MISC).maxStackSize(1).rarity(Rarity.RARE));
+			setRegistryName("gangstas_paradise");
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Sakadik"));
+		}
+	}
+}
